6b7752aade7c18b59a9ba9f39ddcc4b4f9a9b797
Trien Hong <Trien_Hong>
2022-12-02 04:01:46 -0500

    replaced if statements with try expect

--- ticketmaster_api.py
+++ ticketmaster_api.py
@@ -21,11 +21,6 @@ def getEvents(input):
 
     ticketmaster_response_json = ticketmaster_request.json()
 
-    if "page" in ticketmaster_response_json:
-        if "totalElements" in ticketmaster_response_json["page"]:
-            if ticketmaster_response_json["page"]["totalElements"] == 0:
-                return False
-
     idList = []
     titleList = []
     imageList = []
@@ -33,50 +28,44 @@ def getEvents(input):
     cityList = []
     minPriceList = []
     maxPriceList = []
-    
-    if "_embedded" in ticketmaster_response_json:
-        if "events" in ticketmaster_response_json["_embedded"]:
-            index = 0
-            for x in ticketmaster_response_json["_embedded"]["events"]:
+
+    try:
+        for x in ticketmaster_response_json["_embedded"]["events"]:
+            try:
                 idList.append(x["id"])
-                
-                if "name" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    titleList.append(x["name"])
-                else:
-                    titleList.append("TBD")
-                
-                if "images" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    if "url" in ticketmaster_response_json["_embedded"]["events"][index]["images"][0]:
-                        imageList.append(x["images"][0]["url"])
-                else:
-                    imageList.append("TBD")
+            except KeyError as e:
+                idList.append("TBD")
 
-                if "dates" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    if "start" in ticketmaster_response_json["_embedded"]["events"][index]["dates"]:
-                        if "localDate" in ticketmaster_response_json["_embedded"]["events"][index]["dates"]["start"]:
-                            dateList.append(x["dates"]["start"]["localDate"])
-                else:
-                    dateList.append("TBD")
-                
-                if "_embedded" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    if "venues" in ticketmaster_response_json["_embedded"]["events"][index]["_embedded"]:
-                        if "city" in ticketmaster_response_json["_embedded"]["events"][index]["_embedded"]["venues"][0]:
-                            if "name" in ticketmaster_response_json["_embedded"]["events"][index]["_embedded"]["venues"][0]["city"]:
-                                cityList.append(x["_embedded"]["venues"][0]["city"]["name"])
-                else:
-                    cityList.append("TBD")
+            try:
+                titleList.append(x["name"])
+            except KeyError as e:
+                titleList.append("TBD")
+            
+            try:
+                imageList.append(x["images"][0]["url"])
+            except KeyError as e:
+                imageList.append("TBD")
 
-                if "priceRanges" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    if "min" in ticketmaster_response_json["_embedded"]["events"][index]["priceRanges"][0]:
-                        minPriceList.append("$" + str(x["priceRanges"][0]["min"]))
-                else:
-                    minPriceList.append("TBD")
+            try:
+                dateList.append(x["dates"]["start"]["localDate"])
+            except KeyError as e:
+                dateList.append("TBD")
 
-                if "priceRanges" in ticketmaster_response_json["_embedded"]["events"][index]:
-                    if "max" in ticketmaster_response_json["_embedded"]["events"][index]["priceRanges"][0]:
-                        maxPriceList.append("$" + str(x["priceRanges"][0]["max"]))
-                else:
-                    maxPriceList.append("TBD")
-                index = index + 1
+            try:
+                cityList.append(x["_embedded"]["venues"][0]["city"]["name"])
+            except KeyError as e:
+                cityList.append("TBD")
+            
+            try:
+                minPriceList.append("$" + str(x["priceRanges"][0]["min"]))
+            except KeyError as e:
+                minPriceList.append("TBD")
+
+            try:
+                maxPriceList.append("$" + str(x["priceRanges"][0]["max"]))
+            except KeyError as e:
+                maxPriceList.append("TBD")
+    except KeyError as e:
+        return False
 
     return zip(idList, titleList, imageList, dateList, cityList, minPriceList, maxPriceList)
